dffa98c97808618fde04470c47c0192f501d1e0c
Krystian <krystian.parma@gmail.com>
2013-10-31 08:56:36 -0700

    First template

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,4 +1,13 @@
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello world")
+	title = 'hello Dawid'
+	content = 'Hello world & Dawid'
+	t = get_template('base.html')
+	html = t.render(Context({
+		'title': title, 
+		'content': content
+		}))
+	return HttpResponse(html)
--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,6 @@
 # Django settings for main project.
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__) + '../../../')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -108,9 +110,7 @@ ROOT_URLCONF = 'main.urls'
 WSGI_APPLICATION = 'main.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates/'),
 )
 
 INSTALLED_APPS = (
