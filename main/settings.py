67ff6208ee788ca77107e843e5b87eb0226144a4
Fajkowsky <dawid.fajkowski@skygate.pl>
2013-11-06 14:03:35 +0100

    added south and landingpage in installed apps

--- main/settings.py
+++ main/settings.py
@@ -122,6 +122,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'landingpage',
+    #---------------------------
+    'south',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
